afed630f6e58c436ca744cbbd40f5e07c019b754
Tristan Gonzalez Branca <trgonzalez@itba.edu.ar>
2025-06-20 16:15:40 -0300

    aas

--- src/layers/main.layer.ts
+++ src/layers/main.layer.ts
@@ -3,18 +3,24 @@ import { flowSeller } from "../flows/seller.flow"
 import { flowSchedule } from "../flows/schedule.flow"
 import { flowConfirm } from "../flows/confirm.flow"
 import { flowTest } from "../flows/test.flow"
-/**
- * Determina que flujo va a iniciarse basado en el mensaje recibido
- */
-export default async (ctx: BotContext, { gotoFlow }: BotMethods) => {
+
+export default async (ctx: BotContext, { gotoFlow, flowDynamic }: BotMethods) => {
     const message = ctx.body?.trim().toLowerCase();
-    
 
     if (message === '/info') {
         return gotoFlow(flowTest);
-    } else if (message === 'ver horarios') {
+    } else if (message === 'ver estado' || message === 'estado') {
         return gotoFlow(flowTest);
     }
-    
-    // Más condiciones...
+
+    // Si no reconoce el comando, muestra el menú
+    await flowDynamic([
+        {
+            body: "Menú principal",
+            buttons: [
+                [{ body: "Ver Estado" }, { body: "Actuadores" }],
+                [{ body: "Vincular" }, { body: "Actualizar Disp." }]
+            ]
+        }
+    ]);
 }
